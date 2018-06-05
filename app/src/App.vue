3b65be8d959cba743ca75495259004b6b9c4ae74
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2018-06-05 14:45:32 -0700

    app scaffolding built

---
+++ app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <main>
+      <Neighborhoods/>
+    </main>
+  </div>
+</template>
+
+<script>
+import Neighborhoods from './components/Neighborhoods.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Neighborhoods
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
